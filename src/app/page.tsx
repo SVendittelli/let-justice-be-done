9e6d0ed36bf07ad7d74c83b122f7a1979e6987e1
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-06-22 21:07:30 +0100

    feat: adjust text arrangement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,17 @@ import Image from "next/image";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#a4112e] to-[#5a142c] text-[#f5b26c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Vincent
-        </h1>
-        <div className="flex gap-12">
-          <div>
-            <Image
-              src="https://placehold.co/400x500/png"
-              alt="Character Portrait"
-              height="500"
-              width="400"
-            />
-          </div>
+      <div className="container flex gap-12 px-4 py-16">
+        <Image
+          src="https://placehold.co/400x500/png"
+          alt="Character Portrait"
+          height="500"
+          width="400"
+        />
+        <div className="flex flex-grow flex-col gap-12">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Vincent
+          </h1>
           <p className="text-3xl">A talented artist with a dark past.</p>
         </div>
       </div>
