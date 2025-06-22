c8eb179fe5f02834851d4bd1b980127a3a2a84e4
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-06-22 23:58:04 +0100

    feat: add different favicon for dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,22 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { env } from "~/env";
 
 export const metadata: Metadata = {
   title: "Let Justice Be Done",
   description: "Let Justice Be Done",
   icons: [
-    { rel: "icon", url: "/favicon.ico", sizes: "32x32" },
-    { rel: "icon", url: "/icon.svg", type: "image/svg+xml" },
+    {
+      rel: "icon",
+      url: env.NODE_ENV === "production" ? "/favicon.ico" : "/favicon-dev.ico",
+      sizes: "32x32",
+    },
+    {
+      rel: "icon",
+      url: env.NODE_ENV === "production" ? "/icon.svg" : "/icon-dev.svg",
+      type: "image/svg+xml",
+    },
     { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
   ],
 };
