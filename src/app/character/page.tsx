7cfed4ca20bad6855d9de6c17208290a46279f49
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-06-24 18:50:55 +0100

    feat: add homepage

---
+++ src/app/character/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Message from "./_components/message";
+import { HydrateClient } from "~/trpc/server";
+import { auth } from "~/server/auth";
+import { SessionProvider } from "next-auth/react";
+import Link from "next/link";
+
+export default async function Page() {
+  const session = await auth();
+
+  return (
+    <HydrateClient>
+      <Link href="/">
+        <Image
+          src="/icon.svg"
+          alt="Logo"
+          width={50}
+          height={50}
+          className="absolute top-5 left-5 rounded-full border-2 border-solid border-[#f5b26c]"
+        />
+      </Link>
+      <main
+        className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#a4112e] to-[#5a142c] text-[#f5b26c]"
+        role="main"
+      >
+        <div className="container flex flex-col-reverse gap-6 px-4 py-16 sm:flex-row sm:gap-12">
+          <div>
+            <Image
+              src="https://placehold.co/400x500/png"
+              alt="Character Portrait"
+              height="500"
+              width="400"
+            />
+          </div>
+          <div className="flex flex-grow flex-col gap-6 sm:gap-12">
+            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+              Vincent
+            </h1>
+            <p className="text-2xl sm:text-3xl">
+              A talented artist with a dark past.
+            </p>
+            <SessionProvider>{session?.user && <Message />}</SessionProvider>
+          </div>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,33 @@
-import Image from "next/image";
-import Message from "./_components/message";
-import { HydrateClient } from "~/trpc/server";
-import { auth } from "~/server/auth";
 import Link from "next/link";
-import { SessionProvider } from "next-auth/react";
+import { auth } from "~/server/auth";
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <HydrateClient>
-      <main
-        className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#a4112e] to-[#5a142c] text-[#f5b26c]"
-        role="main"
+    <main
+      className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gradient-to-b from-[#a4112e] to-[#5a142c] text-[#f5b26c]"
+      role="main"
+    >
+      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        Let Justice Be Done
+      </h1>
+      {session?.user ? (
+        <>
+          <Link
+            href="/character"
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          >
+            Character
+          </Link>
+        </>
+      ) : null}
+      <Link
+        href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
       >
-        <div className="container flex flex-col-reverse gap-6 px-4 py-16 sm:flex-row sm:gap-12">
-          <div>
-            <Image
-              src="https://placehold.co/400x500/png"
-              alt="Character Portrait"
-              height="500"
-              width="400"
-            />
-          </div>
-          <div className="flex flex-grow flex-col gap-6 sm:gap-12">
-            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-              Vincent
-            </h1>
-            <p className="text-2xl sm:text-3xl">
-              A talented artist with a dark past.
-            </p>
-            <SessionProvider>{session?.user && <Message />}</SessionProvider>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
-      </main>
-    </HydrateClient>
+        {session ? "Sign out" : "Sign in"}
+      </Link>
+    </main>
   );
 }
