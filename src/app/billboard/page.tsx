be2fe3ca3cdb28ddb68ed8d68ef9f85c5bfe40c7
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-07-01 16:20:31 +0100

    feat: add qr code

--- src/app/billboard/page.tsx
+++ src/app/billboard/page.tsx
@@ -4,11 +4,11 @@ export default function Billboard() {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <Image
-        className="aspect-square max-h-[75vh] max-w-[75vw] rounded-full border-8 border-gold object-contain"
+        className="aspect-square max-h-[256px] max-w-[256px] rounded-full border-8 border-gold object-contain"
         src="/icon.svg"
         alt="Let Justice Be Done logo"
-        width={500}
-        height={500}
+        width={256}
+        height={256}
       />
     </div>
   );
---
+++ src/app/billboard/qr/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import QRCode from "react-qr-code";
+
+export default function Page() {
+  const url = window.location.origin;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <QRCode value={url} bgColor="transparent" fgColor="#f5b26c" size={256} />
+    </div>
+  );
+}
