6622e04deb981acb60747ac00814b14bb99e0529
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-07-01 22:20:44 +0100

    fix: only access window on client

--- src/app/billboard/qr/page.tsx
+++ src/app/billboard/qr/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
 
 export default function Page() {
-  const url = window.location.origin;
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    setUrl(window.location.origin);
+  }, [setUrl]);
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <QRCode value={url} bgColor="transparent" fgColor="#f5b26c" size={256} />
+      {url && (
+        <QRCode
+          value={url}
+          bgColor="transparent"
+          fgColor="#f5b26c"
+          size={256}
+        />
+      )}
     </div>
   );
 }
